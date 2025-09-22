328be14a204d9b5973408e6e9414f683e7ff9c21
LaziukAlena <Lazyuk.elena1988@gmail.com>
2025-09-23 01:50:05 +0300

    урок 4

---
+++ src/app/constants/films.constants.ts
@@ -0,0 +1,62 @@
+import { Film } from "../models/film.model"
+export const FILMS: Film[] = [
+    {
+        id: 0,
+        title: "Марсианин",
+        original: "The Martian 2015",
+        category: "Фантастика, приключения",
+        rating: 7.8,
+        link: '/film.html',
+        image: "./images/film.png",
+      },
+    
+      {
+        id: 1,
+        title: "Марсианин",
+        original: "The Martian 2015",
+        category: "Фантастика, приключения",
+        rating: 7.8,
+        link: '/film.html',
+        image: "./images/film.png",
+      },
+    
+      {
+        id: 2,
+        title: "Марсианин",
+        original: "The Martian 2015",
+        category: "Фантастика, приключения",
+        rating: 7.8,
+        link: '/film.html',
+        image: "./images/film.png",
+      },
+    
+      {
+        id: 3,
+        title: "Марсианин",
+        original: "The Martian 2015",
+        category: "Фантастика, приключения",
+        rating: 7.8,
+        link: '/film.html',
+        image: "./images/film.png",
+      },
+    
+      {
+        id: 4,
+        title: "Марсианин",
+        original: "The Martian 2015",
+        category: "Фантастика, приключения",
+        rating: 7.8,
+        link: '/film.html',
+        image: "./images/film.png",
+      },
+    
+      {
+        id: 5,
+        title: "Марсианин",
+        original: "The Martian 2015",
+        category: "Фантастика, приключения",
+        rating: 7.8,
+        link: '/film.html',
+        image: "./images/film.png",
+      },
+]
